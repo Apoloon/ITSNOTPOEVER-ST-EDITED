58000823ab813f1b5f5ccbde74ff37972422ea8e
Apoloon <141363943+Apoloon@users.noreply.github.com>
2023-08-03 16:27:59 -0300

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ export const WHITELISTED_IPS = [
 ];
 // Prompt Moderation before sending to OpenAI
 export const MODERATION = false; // Moderation mode (Set to true to enable content moderation)
-export let COOKIE = process.env.COOKIE || "Your cookie here"; // User cookie value
-export let BROWSER = process.env.BROWSER || "edge"; // Browser type, defaults to "edge"
+export let COOKIE = process.env.COOKIE || "bp6NOR8LBglaGWg7y2hqlw%3D%3D"; // User cookie value
+export let BROWSER = process.env.BROWSER || "edge"; // Browser type, defaults to "chrome"
 export let WEBDRIVERMODE = process.env.DRIVERMODE == "true" || true; // Web driver mode (Set to true to enable web driver mode)
